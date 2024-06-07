e4a15433e0a09ad1ee07d33e50eac18c2f3e2487
Vadym <vadimiaroshenko@gmail.com>
2024-06-07 18:23:45 +0300

    task1 updated

--- hw_5.1.py
+++ hw_5.1.py
@@ -1,6 +1,23 @@
 'hw_5.1'
 import string
 import keyword
-variable = input('Name a vaiable: ')
-is_valid = (not variable[0].isdigit() and not variable.isupper() and (char not in string.punctuation for char in variable) and variable.count('_') <= 1 and variable not in keyword.kwlist)
+is_valid = True
+variable_name = input('Еnter the name for a variable: ')
+if variable_name[0].isdigit():
+    is_valid = False
+if variable_name.count('_') > 1 and is_valid:
+    is_valid = False
+if variable_name in keyword.kwlist and is_valid:
+    is_valid = False
+if is_valid:
+    for char in variable_name:
+        if char in string.punctuation and char != '_':
+            is_valid = False
+            break
+if is_valid:
+    for char in variable_name:
+        if char.isupper():
+            is_valid = False
+            break
+
 print(is_valid)
